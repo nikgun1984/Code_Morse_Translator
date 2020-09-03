b018de8b8f2fbcbf2b054da0db7183b435cb614c
Nick Gundobin <ngundobin@gmail.com>
2020-09-03 16:23:07 -0400

    initial commit

---
+++ english_to_morse_translator.py
@@ -0,0 +1,33 @@
+# Dictionary representing the morse code chart 
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...', 
+                    'C':'-.-.', 'D':'-..', 'E':'.', 
+                    'F':'..-.', 'G':'--.', 'H':'....', 
+                    'I':'..', 'J':'.---', 'K':'-.-', 
+                    'L':'.-..', 'M':'--', 'N':'-.', 
+                    'O':'---', 'P':'.--.', 'Q':'--.-', 
+                    'R':'.-.', 'S':'...', 'T':'-', 
+                    'U':'..-', 'V':'...-', 'W':'.--', 
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..', 
+                    '1':'.----', '2':'..---', '3':'...--', 
+                    '4':'....-', '5':'.....', '6':'-....', 
+                    '7':'--...', '8':'---..', '9':'----.', 
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-', 
+                    '?':'..--..', '/':'-..-.', '-':'-....-', 
+                    '(':'-.--.', ')':'-.--.-'} 
+
+def encrypt_message(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+            cipher += MORSE_CODE_DICT[letter.upper()]+' '
+        else:
+            cipher += '\n'
+    return cipher
+
+
+# def decrypt_message(message):
+message = encrypt_message('My name is Nikolay Gundobin.')
+print(message)
+file = open('code.txt','w')
+file.write(message)
+file.close()
---
+++ morse_to_english_translator.py
@@ -0,0 +1,5 @@
+from english_to_morse_translator import MORSE_CODE_DICT
+
+# def decrypt_message(message):
+#     decipher = ''
+#     for 
